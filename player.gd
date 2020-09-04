81d62cbeccdd15084c435e89d314ad32e8cea6b4
siema <matzzeyy@gmail.com>
2020-09-04 23:29:38 +0200

    add better movement

--- player.gd
+++ player.gd
@@ -1,6 +1,26 @@
 extends KinematicBody2D
 
-func _process(delta):
-	
-		$AnimatedSprite.play("run")
+export (int) var speed = 200
 
+var velocity = Vector2()
+
+func get_input():
+	velocity = Vector2()
+	if Input.is_action_pressed('ui_right'):
+		velocity.x += 1
+		$AnimatedSprite.play("run")
+	
+	elif Input.is_action_pressed('ui_left'):
+		velocity.x -= 1
+		$AnimatedSprite.play("run")
+	else:
+		$AnimatedSprite.stop()
+	
+	velocity = velocity.normalized() * speed
+
+func _physics_process(delta):
+	get_input()
+	velocity = move_and_slide(velocity)
+	
+	
+	
